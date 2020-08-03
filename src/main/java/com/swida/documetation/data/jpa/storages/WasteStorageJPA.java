ec6bae57a3229dfdc752bcb7812b9c6f8833f7ca
vova99 <mr.oleksiyenko@gmail.com>
2020-08-03 16:17:15 +0300

    Add request to Drying Storage OAK

--- src/main/java/com/swida/documetation/data/jpa/storages/WasteStorageJPA.java
+++ src/main/java/com/swida/documetation/data/jpa/storages/WasteStorageJPA.java
@@ -2,6 +2,11 @@ package com.swida.documetation.data.jpa.storages;
 
 import com.swida.documetation.data.entity.storages.WasteStorage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WasteStorageJPA extends JpaRepository<WasteStorage,Integer> {
+    @Query("select r from  WasteStorage  r where r.breedOfTree.id=?1 and  r.userCompany.id=?2")
+    List<WasteStorage> getListByUserByBreed(int breedId, int userId);
 }
