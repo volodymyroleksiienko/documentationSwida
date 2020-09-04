240e33c8e12aea5ccdf7438f1d9e77ada99e91b4
romantymochko@gmail.com <22031998poma>
2020-09-04 15:44:54 +0200

    return to income

---
+++ src/main/resources/static/js/fabric/raw/raw.storage.oak.js
@@ -0,0 +1,182 @@
+function sendForDryingOak(btnObj) {
+
+    var trObj = btnObj.parentElement.parentElement;
+    var trId =  $(trObj).attr('id');
+    $('#sendForDryingModalId').val(trId);
+    console.log(  $(trObj).attr('id'));
+    var codeOfRawStorage = $(trObj).find('th:eq(0)').text();
+    $('#sendForDryingModalHeader').text("Отправить в сушку "+codeOfRawStorage);
+
+    var code =              $(trObj).find('th:eq(0)').text();
+    var material =          $(trObj).find('td:eq(0)').text();
+    var breedDescription =  $(trObj).find('td:eq(1)').text();
+    var thickness =         $(trObj).find('td:eq(2)').text();
+    var extent =            $(trObj).find('td:eq(3)').text();
+    var description =       $(trObj).find('td:eq(4)').text();
+
+
+    $('#sendForDryingModalCode').val(code);
+    $('#sendForDryingModalMaterial').val(material);
+    $('#sendForDryingModalMaterialDescr').val(breedDescription);
+    $('#sendForDryingModalThickness').val(thickness);
+    $('#sendForDryingModalVolume').val(extent);
+    $('#sendForDryingModalDescription').val(description);
+
+
+    $('#sendForDryingModalOak').modal('show');
+
+    $( "#sendForDryingModalFormOak" ).submit(function( event ) {
+        // alert( "Handler for .submit() called." );
+        let maxAmount =		parseFloat(extent);
+        let amount = 		parseFloat($('#sendForDryingModalVolume').val());
+        let luck =			amount-maxAmount;
+
+        if (((parseFloat($('#sendForDryingModalVolume').val()))<=0.0)) {
+            alert("Введите правильное значение");
+            $('#sendForDryingModalVolume').val("0.0");
+        }else if (amount>maxAmount) {
+            alert("Не хватает: "+luck+" м3 !");
+        } else {
+            return;
+        }
+        event.preventDefault();
+    });
+}
+function editRawStorageOak(btnObj) {
+
+    var trObj = btnObj.parentElement.parentElement;
+    var trId =  $(trObj).attr('id');
+    $('#editDryingModalId').val(trId);
+    console.log(  $(trObj).attr('id'));
+    var codeOfRawStorage = $(trObj).find('th:eq(0)').text();
+    $('#editDryingModalHeader').text("Редактировать "+codeOfRawStorage);
+
+    var code = $(trObj).find('th:eq(0)').text();
+    var material = $(trObj).find('td:eq(0)').text();
+    var description = $(trObj).find('td:eq(1)').text();
+    var thickness = $(trObj).find('td:eq(2)').text();
+    var extent = $(trObj).find('td:eq(3)').text();
+    var desc = $(trObj).find('td:eq(4)').text();
+
+    $('#editDryingModalCode').val(code);
+    $('#editDryingModalMaterial').val(material);
+    $('#editDryingModalMaterialDescr').val(description);
+    $('#editDryingModalThickness').val(thickness);
+    $('#editDryingModalVolume').val(extent);
+    $('#editForDryingModalDescription').val(desc);
+
+
+    $('#editDryingModal').modal('show');
+}
+
+
+function sendForPackagesStorageOak(btnObj) {
+    var trObj = btnObj.parentElement.parentElement;
+    var trId =  $(trObj).attr('id');
+    $('#sendForDryingModalIdOak').val(trId);
+    console.log($(trObj).attr('id'));
+    var codeOfRawStorage =      $(trObj).find('th:eq(0)').text();
+    var size =                  $(trObj).find('td:eq(2)').text();
+
+
+    $('#sendForPackageModalOakHeader').text("Формирование пачек из  "+codeOfRawStorage);
+
+    $('#sendFromRawToPackageOakModal').modal('show');
+
+    $('#sendForPackageModalSizeOak').val(size);
+
+    // CHECK
+    $("#sendForPackageModalSizeOak").change(function(){
+        if ((parseInt($('#sendForPackageModalSizeOak').val()))<=0) {
+            alert("Введите правильное значение");
+            $('#sendForPackageModalSizeOak').val("1");
+        }else{
+
+        }
+    });
+
+
+    $("#sendForPackageModalLengthOak").change(function(){
+        if ((parseInt($('#sendForPackageModalLengthOak').val()))<=0) {
+            alert("Введите правильное значение");
+            $('#sendForPackageModalLengthOak').val("1");
+        }else{
+
+        }
+    });
+    // CHECK
+}
+
+
+// ADD Package OAK
+function sendRequestCreatePackageOak(btnObj) {
+    var breedID = $("#breedId").val();
+    var userID = $("#userId").val();
+    var idOfDryStorageOak = $("#sendForDryingModalIdOak").val();
+    console.log("id " + idOfDryStorageOak);
+    var codeOfPackage1 = $('#sendForPackageModalCodeOak').val();
+    var quality1 = $('#sendForPackageModalQualityOak').val();
+    var sizeOfHeight1 = $('#sendForPackageModalSizeOak').val();
+    var long1 = $('#sendForPackageModalLengthOak').val();
+
+    var tableBody = document.getElementById('listOfPackageId');
+    var listTr = tableBody.getElementsByTagName('tr');
+
+    var arrOfDesk = [];
+
+    //fix one dimension array on controller
+    arrOfDesk[0] = [];
+    arrOfDesk[0][0] = "test";
+    arrOfDesk[0][1] = "test";
+
+
+
+    if (codeOfPackage1 != "" && quality1 != "" && sizeOfHeight1 != "" && long1 != "" ) {
+        for (var i = 1; i <= listTr.length; i++) {
+            var width = $(listTr[i - 1]).find('td:eq(0)').text();
+            var count = $(listTr[i - 1]).find('td:eq(1)').text();
+
+            arrOfDesk[i] = [];
+            arrOfDesk[i][0] = width;
+            arrOfDesk[i][1] = count;
+        }
+
+        console.log(arrOfDesk);
+
+        $.ajax({
+            method: "post",
+            url: "/createRawPackageOakObject-" + userID + "-" + breedID,
+            contextType: "application/json",
+            data: {
+                idOfDryStorage: idOfDryStorageOak,
+                codeOfPackage: codeOfPackage1,
+                quality: quality1,
+                sizeOfHeight: sizeOfHeight1,
+                length: long1,
+                arrayOfDesk: arrOfDesk
+            },
+            traditional: true,
+            success: function () {
+                location.reload();
+            },
+            error: function () {
+                alert("Error");
+            }
+        });
+    } else {
+        alert("Заполните все поля!");
+    }
+}
+
+function returnToIncome(btnObj) {
+    let trObj = btnObj.parentElement.parentElement;
+    let trId =  $(trObj).attr('id');
+    $('#returnItemId').val(trId);
+    console.log("row id: "+trId);
+
+    let code = $(trObj).find('th:eq(0)').text();
+
+    $('#returnItemModalConfirmation').text(" Вы уверены, что хотите возвратить партию №"+code+" на склад прихода?");
+    $('#returnToIncomeModal').modal('show');
+}
+
---
+++ src/main/resources/static/js/fabric/raw/raw.storage.pine.js
@@ -0,0 +1,187 @@
+function sendForDrying(btnObj) {
+
+    var trObj = btnObj.parentElement.parentElement;
+    var trId = $(trObj).attr('id');
+    $('#sendForDryingModalId').val(trId);
+    console.log($(trObj).attr('id'));
+    var codeOfRawStorage = $(trObj).find('th:eq(0)').text();
+    $('#sendForDryingModalHeader').text("Отправить в сушку " + codeOfRawStorage);
+
+    var code = $(trObj).find('th:eq(0)').text();
+    var material = $(trObj).find('td:eq(0)').text();
+    var description = $(trObj).find('td:eq(1)').text();
+    var thickness = $(trObj).find('td:eq(2)').text();
+    var width = $(trObj).find('td:eq(3)').text();
+    var length = $(trObj).find('td:eq(4)').text();
+    var count = $(trObj).find('td:eq(5)').text();
+    var extent = $(trObj).find('td:eq(6)').text();
+
+    $('#sendForDryingModalCode').val(code);
+    $('#sendForDryingModalMaterial').val(material);
+    $('#sendForDryingModalMaterialDescr').val(description);
+    $('#sendForDryingModalThickness').val(thickness);
+    $('#sendForDryingModalWidth').val(width);
+    $('#sendForDryingModalLength').val(length);
+    $('#sendForDryingModalCount').val(count);
+    $('#sendForDryingModalVolume').val(extent);
+
+
+    $('#sendForDryingModal').modal('show');
+
+    // CHECK
+    // $( "#sendForDryingModalFormPine" ).submit(function( event ) {
+    //     // alert( "Handler for .submit() called." );
+    //     let maxAmount =		parseInt(count);
+    //     let amount = 		parseFloat($('#sendForDryingModalCount').val());
+    //     let luck =			amount-maxAmount;
+    //
+    //     if (((parseFloat($('#sendForDryingModalCount').val()))<=0.0)) {
+    //         alert("Введите правильное значение");
+    //         $('#sendForDryingModalCount').val("0.0");
+    //     }else if (amount>maxAmount) {
+    //         alert("Не хватает: "+luck+" м3 !");
+    //     } else {
+    //         return;
+    //     }
+    //     event.preventDefault();
+    // });
+
+    $("#sendForDryingModalCount").change(function () {
+        if ((parseInt($('#sendForDryingModalCount').val())) <= 0) {
+            alert("Введите правильное значение");
+            $('#sendForPackageModalPacksCount').val("1");
+        } else {
+            var maxAmount = count;
+            var amount = parseInt($('#sendForDryingModalCount').val());
+            var luck = amount - maxAmount;
+            if (amount > maxAmount) {
+                alert("Не хватает досок: " + luck + "!");
+                $('#sendForDryingModalCount').val("");
+            }
+        }
+    });
+    // CHECK
+}
+
+
+function editRawStorage(btnObj) {
+
+    var trObj = btnObj.parentElement.parentElement;
+    var trId =  $(trObj).attr('id');
+    $('#editDryingModalId').val(trId);
+    console.log(  $(trObj).attr('id'));
+    var codeOfRawStorage = $(trObj).find('th:eq(0)').text();
+    $('#editDryingModalHeader').text("Редактировать "+codeOfRawStorage);
+
+    var code = $(trObj).find('th:eq(0)').text();
+    var material = $(trObj).find('td:eq(0)').text();
+    var description = $(trObj).find('td:eq(1)').text();
+    var thickness = $(trObj).find('td:eq(2)').text();
+    var width = $(trObj).find('td:eq(3)').text();
+    var length = $(trObj).find('td:eq(4)').text();
+    var count = $(trObj).find('td:eq(5)').text();
+    var extent = $(trObj).find('td:eq(6)').text();
+
+    $('#editDryingModalCode').val(code);
+    $('#editDryingModalMaterial').val(material);
+    $('#editDryingModalMaterialDescr').val(description);
+    $('#editDryingModalThickness').val(thickness);
+    $('#editDryingModalWidth').val(width);
+    $('#editDryingModalLength').val(length);
+    $('#editDryingModalCount').val(count);
+    $('#editDryingModalVolume').val(extent);
+
+
+    $('#editDryingModal').modal('show');
+}
+
+function sendForPackagesStorage(btnObj) {
+
+    var trObj = btnObj.parentElement.parentElement;
+    var trId =  $(trObj).attr('id');
+    $('#sendForPackagesModalId').val(trId);
+    console.log(  $(trObj).attr('id'));
+    var codeOfRawStorage = $(trObj).find('th:eq(0)').text();
+    $('#sendForPackageModalHeader').text("Формирование пачек из  "+codeOfRawStorage);
+
+    var code = 			$(trObj).find('th:eq(0)').text();
+    var material = 		$(trObj).find('td:eq(0)').text();
+    var description = 	$(trObj).find('td:eq(1)').text();
+    var thickness = 	parseInt($(trObj).find('td:eq(2)').text());
+    var width = 		parseInt($(trObj).find('td:eq(3)').text());
+    var length = 		parseInt($(trObj).find('td:eq(4)').text());
+    var count = 		parseInt($(trObj).find('td:eq(5)').text());
+    var extent = 		$(trObj).find('td:eq(6)').text();
+
+    $('#sendForPackageModalCode')			.val(code);
+    $('#sendForPackageModalMaterial')		.val(material);
+    $('#sendForPackageModalMaterialDescr')	.val(description);
+    $('#sendForPackageModalThickness')		.val(thickness);
+    $('#sendForPackageModalWidth')			.val(width);
+    $('#sendForPackageModalLength')			.val(length);
+    $('#sendForPackageModalCount')			.val(count);
+    $('#sendForPackageModalVolume')			.val(extent);
+
+    $('#sendFromRawToPackageModal').modal('show');
+
+    // CHECK
+    $("#sendForPackageHeightPc").change(function(){
+        if ((parseInt($('#sendForPackageHeightPc').val()))<=0) {
+            alert("Введите правильное значение");
+            $('#sendForPackageHeightPc').val("1");
+        }else{
+
+        }
+    });
+
+    $("#sendForPackageModalWidthPc").change(function(){
+        if ((parseInt($('#sendForPackageModalWidthPc').val()))<=0) {
+            alert("Введите правильное значение");
+            $('#sendForPackageModalWidthPc').val("1");
+        }else{
+
+        }
+    });
+
+    $("#sendForPackageModalLengthFact").change(function(){
+        if ((parseInt($('#sendForPackageModalLengthFact').val()))<=0) {
+            alert("Введите правильное значение");
+            $('#sendForPackageModalLengthFact').val("1");
+        }else{
+
+        }
+    });
+
+    $("#sendForPackageModalPacksCount").change(function(){
+        if ((parseInt($('#sendForPackageModalPacksCount').val()))<=0) {
+            alert("Введите правильное значение");
+            $('#sendForPackageModalPacksCount').val("1");
+        }else{
+            var heightPc = 		parseInt($('#sendForPackageHeightPc').val());
+            var widthPc = 		parseInt($('#sendForPackageModalWidthPc').val());
+            var amount = 		heightPc*widthPc*(parseInt($('#sendForPackageModalPacksCount').val()));
+            var maxAmount =		count;
+            var packsCt = 		parseInt($('#sendForPackageModalPacksCount').val());
+            var luck =			amount-maxAmount;
+            if (amount>maxAmount) {
+                alert("Не хватает досок: "+luck+"!");
+                $('#sendForPackageModalPacksCount').val("");
+            }
+        }
+    });
+    // CHECK
+}
+
+
+function returnToIncome(btnObj) {
+    let trObj = btnObj.parentElement.parentElement;
+    let trId =  $(trObj).attr('id');
+    $('#returnItemId').val(trId);
+    console.log("row id: "+trId);
+
+    let code = $(trObj).find('th:eq(0)').text();
+
+    $('#returnItemModalConfirmation').text(" Вы уверены, что хотите возвратить партию №"+code+" на склад прихода?");
+    $('#returnToIncomeModal').modal('show');
+}
+
