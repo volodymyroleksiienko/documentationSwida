99c6da743fa5f206b8bb00fd9b036dbba317c671
vova99 <mr.oleksiyenko@gmail.com>
2020-09-06 22:25:38 +0300

    edit raw storage from left provider

--- src/main/java/com/swida/documetation/data/jpa/OrderInfoJPA.java
+++ src/main/java/com/swida/documetation/data/jpa/OrderInfoJPA.java
@@ -21,6 +21,9 @@ public interface OrderInfoJPA extends JpaRepository<OrderInfo,Integer> {
     @Query("select obj from OrderInfo  obj where obj.contrAgent.id=?1 and obj.statusOfEntity='ACTIVE'")
     List<OrderInfo> getOrdersListByAgent(int agentId);
 
+    @Query("select obj from OrderInfo  obj where obj.breedOfTree.id=?1 and obj.statusOfOrderInfo='DISTRIBUTION' and obj.statusOfEntity='ACTIVE'")
+    List<OrderInfo> getOrdersListByBreed(int breedId);
+
     @Query("select obj from OrderInfo  obj where obj.contrAgent.id=?1 and obj.breedOfTree.id=?2 and obj.statusOfEntity='ACTIVE'")
     List<OrderInfo> getOrdersListByAgentByBreed(int agentId,int breedId);
 
