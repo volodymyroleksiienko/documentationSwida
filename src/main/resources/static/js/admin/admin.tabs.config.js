4c69bccf96c0f58adad5af83c63e15127804b43a
vova99 <mr.oleksiyenko@gmail.com>
2020-09-30 01:38:04 +0300

    export fix bug

--- src/main/resources/static/js/admin/admin.tabs.config.js
+++ src/main/resources/static/js/admin/admin.tabs.config.js
@@ -1,5 +1,6 @@
 $(document).ready( function () {
 
+
     $('#admin-users-table').DataTable({
         "language": {
             "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Russian.json"
