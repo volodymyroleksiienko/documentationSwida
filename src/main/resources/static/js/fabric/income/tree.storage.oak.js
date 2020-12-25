4ee6026bf7b6a9c3feddd79e5b234ed929d3caa8
romantymochko@gmail.com <22031998poma>
2020-12-25 19:55:55 +0100

    Mod

--- src/main/resources/static/js/fabric/income/tree.storage.oak.js
+++ src/main/resources/static/js/fabric/income/tree.storage.oak.js
@@ -4,6 +4,7 @@ function showCutOakModal(btnObj) {
     let trId =  $(trObj).attr('id');
     $('#idOfOakTreeStorageRow').val(trId);
     console.log("row id:" + $(trObj).attr('id'));
+
     let codeOfTreeStorage = $(trObj).find('th:eq(0)').text();
     let description =       $(trObj).find('td:eq(2)').text();
     let extent =            $(trObj).find('td:eq(7)').text();
@@ -12,8 +13,40 @@ function showCutOakModal(btnObj) {
     $('#cutOakRawStorageCode').val(codeOfTreeStorage);
     $('#cutOakRawStorageBreedDesc').val(description);
     $('#maxPossibleOakExtent').val(extent);
+
     $('#cutOakModal').modal('show');
 }
+
+$( "#cutOakModalForm" ).submit(function( event ) {
+
+    let rawStorage =            parseFloat($('#cutOakRawStorageExtent').val());
+    let used =                  parseFloat($('#cutOakRawStorageUsedExtent').val());
+    let extentOfWaste =         parseFloat($('#extentOfWasteOak').val());
+
+    let maxVal =                 parseFloat($('#maxPossibleOakExtent').val());
+
+    console.log("calc: "+(rawStorage+extentOfWaste));
+    console.log("max: "+maxVal);
+
+    if ((rawStorage+extentOfWaste)>maxVal) {
+        alert("Кубатура после распиловки и отходов превишает максимально возможную на "+((rawStorage+extentOfWaste)-maxVal)+" м3.");
+
+        // $('#cutOakRawStorageExtent').val(0.000);
+        // $('#cutOakRawStorageUsedExtent').val(0.000);
+        // $('#extentOfWasteOak').val(0.000);
+
+        return false;
+    } else if (used>maxVal){
+        alert("Кубатура использованого материала превишает максимально возможную на "+(used-maxVal)+" м3.");
+        return false;
+    } else  if ((rawStorage+extentOfWaste)>used || (rawStorage+extentOfWaste)<used){
+        alert("Кубатура использованого материала не сходится с суммой кубатур после распиловки и отходами.");
+        return false;
+    } else {
+        return true;
+    };
+    event.preventDefault();
+});
 
 function showEditOakModal(btnObj) {
     let trObj = btnObj.parentElement.parentElement;
