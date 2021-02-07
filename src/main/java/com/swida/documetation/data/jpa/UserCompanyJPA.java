5b37b2182cd9636ba3d1829ac26646ce4b5bc842
vova99 <mr.oleksiyenko@gmail.com>
2021-02-07 13:28:08 +0200

    Fix problem

--- src/main/java/com/swida/documetation/data/jpa/UserCompanyJPA.java
+++ src/main/java/com/swida/documetation/data/jpa/UserCompanyJPA.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.*;
 
 public interface UserCompanyJPA extends JpaRepository<UserCompany,Integer> {
-    @Query("select u from UserCompany u where u.username=?1 group by u.username")
+//    @Query("select u from UserCompany u where u.username=?1")
     UserCompany findByUsername(String username);
 
     @Query("select u from UserCompany  u where u.role<>'ROLE_ADMIN'")
