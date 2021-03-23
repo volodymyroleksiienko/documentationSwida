48e7458cd4c85c1c37ce6b411e86398494bcc91e
vova99 <mr.oleksiyenko@gmail.com>
2021-03-23 16:39:58 +0200

    Fix problem

--- src/main/java/com/swida/documetation/data/entity/storages/QualityStatisticInfo.java
+++ src/main/java/com/swida/documetation/data/entity/storages/QualityStatisticInfo.java
@@ -3,6 +3,9 @@ package com.swida.documetation.data.entity.storages;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Data
@@ -12,9 +15,11 @@ public class QualityStatisticInfo {
     private int id;
 
     public String height="0";
+    public String firstExtent="0";
     public String extent="0";
     public String percent="0";
 
+    public String date=DateTimeFormatter.BASIC_ISO_DATE.format(LocalDate.now());
 
     @ManyToOne
     private TreeStorage treeStorage;
