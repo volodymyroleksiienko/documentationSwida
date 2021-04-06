8a492529c1fc7d5ce6af5131c7658b8e9ebe50d5
romantymochko@gmail.com <22031998poma>
2021-04-06 17:32:04 +0200

    stats

--- src/main/resources/static/js/fabric/dry/dry.storage.oak.js
+++ src/main/resources/static/js/fabric/dry/dry.storage.oak.js
@@ -93,7 +93,7 @@ function editDryStorageOak(btnObj) {
     var length =        $(trObj).find('td:eq(4)').text();
     var extent =        $(trObj).find('td:eq(5)').text();
     var desc =          $(trObj).find('td:eq(6)').text();
-    var dryingExtent =  $(trObj).find('td:eq(8)').text();
+    var dryingExtent =  $(trObj).find('td:eq(9)').text();
     var initialExt =    $(trObj).find('td:eq(9)').text();
 
     console.log(code, material,description,thickness,extent,desc,dryingExtent,initialExt);
