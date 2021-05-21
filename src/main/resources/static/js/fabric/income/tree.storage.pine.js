0bf51e91b5a6e81d2befb0de6130202344526bc6
romantymochko@gmail.com <22031998poma>
2021-05-21 15:33:14 +0200

    tree storage

--- src/main/resources/static/js/fabric/income/tree.storage.pine.js
+++ src/main/resources/static/js/fabric/income/tree.storage.pine.js
@@ -75,30 +75,34 @@ function showEditCutModal(btnObj){
     var trId =  $(trObj).attr('id');
     $('#incomeTreeStorageId').val(trId);
 
-    var code =          $(trObj).find('th:eq(0)').text();
-    var date =          $(trObj).find('td:eq(1)').text();
-    var breed =         $(trObj).find('td:eq(2)').text();
-    var desc =          $(trObj).find('td:eq(3)').text();
-    var provider =      $(trObj).find('td:eq(4)').text();
-    var providerId =    $(trObj).find('td:eq(5)').text();
-    var avgDiameter =   $(trObj).find('td:eq(6)').text();
-    var amount =        $(trObj).find('td:eq(7)').text();
-    var extent =        $(trObj).find('td:eq(8)').text();
-    var initialExtent = $(trObj).find('td:eq(9)').text();
-    // var date =          $(trObj).find('td:eq(9)').text();
-    var description =   $(trObj).find('td:eq(10)').text();
+    var teamCode =      $(trObj).find('td:eq(1)').text();
+    var date =          $(trObj).find('td:eq(0)').text();
+    var desc =          $(trObj).find('td:eq(2)').text();
 
-    $('#editIncomeCode')            .val(code);
-    $('#editIncomeMaterial')        .val(breed);
-    $('#editIncomeMaterialDesc')    .val(desc);
-    $('#editIncomeSupplier')        .val(provider);
-    $('#editIncomeSupplier-hidden') .val(providerId);
-    $('#editAverageDiameter')       .val(avgDiameter);
-    $('#editAmount')                .val(amount);
-    $('#editIncomeVolume')          .val(extent);
-    $('#editIncomeInitialVolume')   .val(initialExtent);
-    $('#editIncomeDate')            .val(date);
-    $('#editIncomeDescription')     .val(description);
+    var thickness = 	$(trObj).find('td:eq(3)').text();
+    var width = 		$(trObj).find('td:eq(4)').text();
+    var length = 		$(trObj).find('td:eq(5)').text();
+    var count = 		$(trObj).find('td:eq(6)').text();
+
+    var usedExtent =    $(trObj).find('td:eq(7)').text();
+    var extent =        $(trObj).find('td:eq(8)').text();
+
+    var addedTo =       $(trObj).find('td:eq(10)').text().trim();
+
+    $('#editTeamCode')              .val(teamCode);
+    $('#editDate')                  .val(date);
+    $('#editMaterialDescr')         .val(desc);
+
+    $('#editThickness')             .val(thickness);
+    $('#editWidth')                 .val(width);
+    $('#editLength')                .val(length);
+    $('#editDescCount')             .val(count);
+
+    $('#editUsedVolume')            .val(usedExtent);
+    $('#editVolume')                .val(extent);
+
+    $('#editAddedTo')               .val(addedTo);
+
 
     $('#editTreeStorageCutModal').modal('show');
 }
