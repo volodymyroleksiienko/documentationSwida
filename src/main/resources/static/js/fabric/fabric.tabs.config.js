c2f94b9bf26cee449c8768888f19b3562e339958
vova99 <mr.oleksiyenko@gmail.com>
2021-06-13 20:53:32 +0300

    fix

--- src/main/resources/static/js/fabric/fabric.tabs.config.js
+++ src/main/resources/static/js/fabric/fabric.tabs.config.js
@@ -799,7 +799,7 @@ $(document).ready( function () {
             exampleLength = firstData[7];
 
             console.log("data 0: "+data[2]);
-            if (data[6]===firstData[6]&&data[7]===firstData[7]&&data[2].toString()===firstData[2].toString() && data[14] === "false" && firstData[14] === "false" && data[15]!=="true" && firstData[15]!=="true"){
+            if (data[6]===firstData[6]&&data[7]===firstData[7]&& data[14] === "false" && firstData[14] === "false" && data[15]!=="true" && firstData[15]!=="true"){
 
                 console.log('ok');
                 extent += (parseFloat(data[8]));
